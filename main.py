af8b8b0c756088ff47a9e021b9eeef302d5e0e92
GaelLaraBautista <larabautistagael@gmail.com>
2025-01-25 18:55:26 -0600

    02 implementando conexión con el frontend

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return{"message":"¡Bienvenido a esta aplicacion en fastapi!"}
+# Permitir el acceso desde el frontend (React)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # URL del frontend
+    allow_methods=["*"],  # Métodos permitidos (GET, POST, etc.)
+    allow_headers=["*"],  # Headers permitidos
+)
+
+# Simulando productos
+fake_products = [
+    {"id": 1, "name": "Producto A", "price": 10.99},
+    {"id": 2, "name": "Producto B", "price": 20.99},
+    {"id": 3, "name": "Producto C", "price": 30.99},
+]
+
+@app.get("/products")
+def get_products():
+    return fake_products
